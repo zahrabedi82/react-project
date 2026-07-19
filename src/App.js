11508d1e4f3408bff2b544760db7457394144bc9
zahrabedi82 <zahra.abedi8262@gmail.com>
2026-07-19 11:58:17 +0330

    feat: create Featured section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
+import Featured from "./components/feature/Featured"
+
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Featured />
     </>
   );
 }
 
 export default App;
+
+
+
+
+//" https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin&x_cg_demo_api_key=CG-RU3dQiTHwQ2TwNwTcRcAcX3V";
